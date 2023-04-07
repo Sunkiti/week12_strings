75890385c9ae30a316248e84c4d4a678aaadfdb6
Sunkiti <elena.bulavkina@gmail.com>
2023-04-08 02:23:37 +0300

    functions

--- index.js
+++ index.js
@@ -6,17 +6,38 @@ const commentsElement = document.getElementById ('comments');
 console.log(commentsElement);
 const btn = document.querySelector('.btn');
 console.log(btn);
+const nameResult = document.getElementById ('nameResult');
+console.log(nameResult);
+const avatarResult = document.getElementById ('avatarResult');
+console.log(avatarResult);
+const commentsResult = document.getElementById ('commentsResult');
+console.log(commentsResult);
 btn.addEventListener('click', () => {
     const userName = nameElement.value;
     console.log(userName);
-    const userAvatar = avatarElement.value;
+    const userAvatar = avatarElement.style.backgroundImage;
     console.log (userAvatar);
     const userComments = commentsElement.value;
     console.log(userComments);
+    nameResult.value = nameElement.value;
+    avatarResult.style.backgroundImage = avatarElement;
+    commentsResult.value = commentsElement;
 })
+// var fileReader = new FileReader();
+// fileReader.onload = functino() {
+//     img1.src = fileReader.result;
+// }
 
-// const commentsResult = document.getElementById ('commentsResult');
-// const avatar = document.getElementById ('avatar');
+// fileReader.readAsDataURL(target.files[0]);
+// document.getElementById("avatar").addEventListener("change", function () {
+//     if (this.files[0]) {
+//     var fr = new FileReader();
+//     fr.addEventListener("load", function () {
+//     document.getElementById("avatarResult").style.backgroundImage = "url(" + fr.result + ")";
+// }, false);
+//     fr.readAsDataURL(this.files[0]);
+//     }
+// });
 
 
 // const block = document.querySelector('.block');
