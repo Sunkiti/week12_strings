b654e7c7bb9ff5a8987e342533b307d7189311c9
Sunkiti <elena.bulavkina@gmail.com>
2023-04-07 21:10:35 +0300

    third commit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const nameElement = document.getElementById ('username').textContent;
+const nameElement = document.getElementById ('username');
 console.log(nameElement);
+const avatarElement = document.getElementById ('avatar');
+console.log(avatarElement);
+const commentsElement = document.getElementById ('comments');
+console.log(commentsElement);
 const btn = document.querySelector('.btn');
 console.log(btn);
 btn.addEventListener('click', () => {
-    const userName = nameElement.textContent;
+    const userName = nameElement.value;
     console.log(userName);
+    const userAvatar = avatarElement.value;
+    console.log (userAvatar);
+    const userComments = commentsElement.value;
+    console.log(userComments);
 })
 
 // const commentsResult = document.getElementById ('commentsResult');
