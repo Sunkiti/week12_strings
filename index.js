780a17208338cdcde2916c00e989dd45f3125859
Sunkiti <elena.bulavkina@gmail.com>
2023-04-16 03:21:55 +0300

    additions

--- index.js
+++ index.js
@@ -12,15 +12,49 @@ const avatarResult = document.getElementById ('avatarResult');
 console.log(avatarResult);
 const commentsResult = document.getElementById ('commentsResult');
 console.log(commentsResult);
+const time = document.getElementById ('time');
+console.log (time);
+const arr = [
+    "/img/1.jpg",
+    "/img/2.jpg",
+    "/img/3.jpg",
+    "/img/4.jpg",
+    "/img/5.jpg",
+    "/img/6.jpg",
+];
+function checkedWord(word) {
+    return `${word.slice(0, 1).toUpperCase()}${word.slice(1).toLowerCase()} `;
+}
 btn.addEventListener('click', () => {
+    const userTime = new Date;
     const userName = nameElement.value;
     console.log(userName);
     const userAvatar = avatarElement.value;
     console.log (userAvatar);
     const userComments = commentsElement.value;
     console.log(userComments);
+    let url = "";
+    if (!avatarElement.value) {
+        const indexArr = Math.floor(Math.random() * 6);
+        console.log();
+        url = arr[indexArr];
+        } else {
+        url = avatarElement.value;
+        }
     nameResult.value = nameElement.value;
-    document.querySelector ('#img').src = userAvatar;
+    const initials = nameResult.value.split (" ");
+    let surname = checkedWord(initials[0]);
+    let nameUser = checkedWord(initials[1]);
+    let patronimic = checkedWord(initials[2]);
+    const totalString = `${surname} ${nameUser} ${patronimic}`;
+    nameResult.value = (`${totalString}`);
+    avatarResult.innerHTML = 
+    `<div class="image">
+        <img
+        src=${url}
+        alt="картинка"
+        class="userAvatar"/>
+    </div>`;
     commentsResult.value = commentsElement.value;
 })
 
